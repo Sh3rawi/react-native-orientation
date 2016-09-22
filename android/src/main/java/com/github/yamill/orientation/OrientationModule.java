392e32b8b02d98f4d7b31682f0129d7eb35a8b09
Hamza Sharawi <hamza.sharawi@gmail.com>
2016-09-22 15:56:11 +0300

    more saber

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -48,6 +48,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
                 }
             }
         };
+
+        ctx.addLifecycleEventListener(this);
     }
 
     @Override
